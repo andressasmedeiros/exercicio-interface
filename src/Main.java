97b037341c7b97aec69cf8f3c52b8c7450448fd2
Andressa Medeiros <andressa.scharaiber@gmail.com>
2025-07-05 14:43:36 -0300

    exercicio que calcula  a área de uma figura geométrica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+
+public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int option;
+        GeometricForm geometricForm = null;
+        while (true) {
+            System.out.println("Escolha a forma geométrica para calcular a área");
+            System.out.println("1 - Quadrado");
+            System.out.println("2 - Retângulo");
+            System.out.println("3 - Círculo");
+            System.out.println("4 - Sair");
+            option = scanner.nextInt();
+            if (option == 1) {
+                geometricForm = createSquare();
+            } else if (option == 2) {
+                geometricForm = createRectangle();
+            } else if (option == 3) {
+                geometricForm = createCircle();
+            } else if (option == 4) {
+                break;
+            } else {
+                System.out.println("Opção inválida");
+                continue;
+            }
+            System.out.println("O resultado da área do cálculo foi de " + geometricForm.getArea());
+        }
+    }
+
+    private static GeometricForm createSquare() {
+        System.out.println("Informa os tamanhos dos lados: ");
+        var side = scanner.nextDouble();
+        return new Square(side);
+    }
+
+    private static GeometricForm createRectangle() {
+        System.out.println("Informa a base: ");
+        var base = scanner.nextDouble();
+        System.out.println("Informe a altura: ");
+        var height = scanner.nextDouble();
+        return new Rectangle(height, base);
+    }
+
+    private static GeometricForm createCircle() {
+        System.out.println("Informa os tamanhos o raios: ");
+        var radius = scanner.nextDouble();
+        return new Circle(radius);
     }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,6 @@
-public record Square() {
+public record Square(double side) implements GeometricForm {
+    @Override
+    public double getArea() {
+        return side * side;
+    }
 }
